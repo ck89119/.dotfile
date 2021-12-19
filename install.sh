4016406a1e65989ec54ed1d02f80e8f02832e2ab
曹凯 <caokai.1109@bytedance.com>
2021-12-19 15:50:09 +0800

    update

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-# install brew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+# if mac os
+if [ `uname` = "Darwin" ]
+then
+    # install homebrew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-# install wget 
-brew install wget
+    # install wget
+    brew install wget
+fi
 
 cd ~
 
@@ -20,25 +24,23 @@ ln -s .dotfile/.gitconfig .gitconfig
 [ -e ".p10k.zsh" ] && rm .p10k.zsh
 ln -s .dotfile/.p10k.zsh .p10k.zsh
 
-# install zsh
-brew install zsh
-
-# change default shell -> zsh
-chsh -s /bin/zsh
-
-# install oh-my-zsh
-wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
+# link .zshrc configuration
 [ -e ".zshrc" ] && rm .zshrc
 ln -s .dotfile/.zshrc .zshrc 
+
+# install oh-my-zsh
+[ -e ".oh-my-zsh" ] && rm -rf .oh-my-zsh
+wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
 
 # install powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # install zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-# install zsh-autosuggestions 
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# change default shell -> zsh
+chsh -s /bin/zsh
+
 
 # install my vimrc
 wget -O - https://raw.github.com/ck89119/.vim/master/auto-install.sh | sh
