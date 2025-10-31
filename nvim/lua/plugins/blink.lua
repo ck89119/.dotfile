5055172173b97e098b220aa992026462d25c53ca
Cao Kai <ck89119@gmail.com>
2025-11-01 03:37:22 +0800

    update

---
+++ nvim/lua/plugins/blink.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        preset = "enter",
+        ["<Tab>"] = { "select_next", "fallback" },
+        ["<S-Tab>"] = { "select_prev", "fallback" },
+      },
+    },
+  },
+}
